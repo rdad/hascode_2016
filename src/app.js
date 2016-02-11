d163d745b777d43c399463904c455dd24847cb8e
JM Clery <jmclery@gmail.com>
2016-02-11 21:56:17 +0100

    Command builder OK

--- src/app.js
+++ src/app.js
@@ -9,4 +9,14 @@
 
 	});
 
+	var commander = new Commander();
+
+	// Examples !
+	commander.load(1, 2, 3, 4);
+	commander.load(5, 6, 7, 8);
+	commander.deliver('a', 'b', 'c', 'd');
+	commander.load(9, 10, 11, 12);
+	console.log(commander.build());
+
+
 })(jQuery);
